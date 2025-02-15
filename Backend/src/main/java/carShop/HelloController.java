740cc50e6edec8e61dce10b46267811cb10b1c30
Elyas <elyasjabari533@gmail.com>
2025-02-15 16:58:27 +0100

    read json data in SpringBoot

--- Backend/src/main/java/carShop/HelloController.java
+++ Backend/src/main/java/carShop/HelloController.java
@@ -1,15 +1,27 @@
 package carShop;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/cars")
 public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, Spring Boot is running!";
+        return "Hallo, Spring Boot is running";
+    }
+
+    @GetMapping("/json")
+    public String getCars() throws IOException {
+        Resource resource = new ClassPathResource("static/cars.json");
+        return new String(Files.readAllBytes(Paths.get(resource.getURI())));
     }
 }
